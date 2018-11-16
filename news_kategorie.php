92e8645ccc6113f93c97b20589f62eb2f65a83c1
PRagus19 <pragus36@gmail.com>
2018-11-16 22:06:00 +0100

    z edycją kategorii newsów

--- news_kategorie.php
+++ news_kategorie.php
@@ -25,7 +25,12 @@ if($akcja=="nowy"){
 } elseif($akcja=="dodaj"){
 	//zapis nowego rekordu do tabeli
 	$rok = date("Y");
-	$lokalizacja = "./zasoby/".$rok."/news_kategorie/news_kategoria.jpg";
+	$sql = "SELECT `katnews_id` FROM `kategorie_news` 
+	ORDER BY `katnews_id` DESC LIMIT 1";
+	$query = mysqli_query($link, $sql);
+	$dane = mysqli_fetch_array($query);
+	$id = $dane['katnews_id'] + 1;
+	$lokalizacja = "./zasoby/".$rok."/news_kategorie/news_kategoria".$id.".jpg";
 	if ($_FILES['obraz']['type'] != 'image/jpeg'){
 		echo'To nie jest obraz .jpg';
 	} else{
@@ -45,10 +50,39 @@ if($akcja=="nowy"){
 	
 } elseif($akcja=="edycja"){
 	//formularz z danymi do edycji
+	$sql = "SELECT * FROM `kategorie_news` WHERE `katnews_id`=".$_GET['id'];
+	$query = mysqli_query($link,$sql);
+	$rekord = mysqli_fetch_array($query);
+	
+	echo'
+		<form enctype="multipart/form-data" action="index.php?akcja=edytuj&id='.$_GET['id'].'" method="post">
+		<label>Nazwa kategorii newsów:
+		<input name="nazwa" type="text" size="30" value="'.$rekord['katnews_nazwa'].'">
+		</label><br>
+		<label>Kolejność:
+		<input name="kolejnosc" type="text" size="3" value="'.$_GET['id'].'">
+		</label><br>
+		<label>Obraz kategorii:
+		<img src="'.$rekord['katnews_obraz'].'">
+		</label><br>
+		<input name="obraz" type="file"><br><br>
+		<input type="submit" value="Zapisz zmiany">
+		
+		</form>
+		';
 	
 } elseif($akcja=="edytuj"){
 	//aktualizacja rekordu w tabeli
-	
+	$sql = 'UPDATE `kategorie_news` SET 
+	`katnews_nazwa`="'.$_POST['nazwa'].'",
+	`katnews_kolejnosc`="'.$_POST['kolejnosc'].'"
+	WHERE `katnews_id`='.$_GET['id'];
+		$query = mysqli_query($link, $sql);
+	if(!$query){
+		echo "Wystąpił błąd przy zmianie kategorii".$_GET["id"];
+	} else {
+		echo "Zmieniono kategorię ".$_GET["id"];
+	}
 } elseif($akcja=="usuń"){
 	//skasowanie rekordu tabeli
 	$sql="DELETE FROM `kategorie_news` WHERE `katnews_id`=".$_GET["id"];
